a3a64792eafcff90ab72654eb6fcba19967e9d6e
Nidheimr <septonixbusiness@gmail.com>
2024-01-02 18:50:20 +0000

    More thorough tests in test.c

--- src/test.c
+++ src/test.c
@@ -1,9 +1,23 @@
 #include "rias.h"
 
+// TEST: main hijack
 int main()
 {
-    int* a = malloc(sizeof(int));
-    free(a);
+    // TEST: hijacked malloc
+    int* m = malloc(sizeof(int));
+
+    // TEST: hijacked calloc
+    int* c = calloc(1, sizeof(int));
+
+    // TEST: hijacked realloc
+    int* r = malloc(sizeof(int));
+    r = realloc(r, 2 * sizeof(int));
+
+    // TEST: hijacked free
+    free(c);
+    free(r);
+
+    // TEST: purposely leak m
 
     return 0;
 }
